27d12bfaa111217601c140b3ff24cda047d24b3c
bossstory <98545932+bossstory@users.noreply.github.com>
2022-02-15 14:33:00 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+  git clone https://github.com/bossstory/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
